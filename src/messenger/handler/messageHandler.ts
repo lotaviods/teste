3080095eeba3ae4fc11582e34daec31d18fc692f
lotaviods <l.otavio9099@gmail.com>
2023-03-08 16:16:00 -0300

    fix: Message handler to log propertly

--- src/messenger/handler/messageHandler.ts
+++ src/messenger/handler/messageHandler.ts
@@ -1,5 +1,5 @@
 import wa from '@open-wa/wa-automate';
 
 export async function handleMessage(client: wa.Client, message: wa.Message) {
-    console.log(`${client}, Author: ${message.author} - ${message}`)
+    console.log(`Author: ${message.sender.name} - ${message.body}`)
 }
